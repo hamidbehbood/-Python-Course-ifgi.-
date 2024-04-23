aae1d237abd0ced5fca9ba3ae292ccbeca96c428
ZuhalYildirim <zuhal.yldrm.20@gmail.com>
2024-04-23 09:11:29 +0200

    Create calculator.py

---
+++ exercise_3/calculator.py
@@ -0,0 +1,9 @@
+class calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        
+        self.addition = addition
+        self.subtraction = subtraction
+        self.multiplication = multiplication
+        self.division = division
