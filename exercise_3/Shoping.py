f137f28fba7bf1679053f5dff3380364658a611d
hamidbehbood <120721881+hamidbehbood@users.noreply.github.com>
2024-04-23 10:07:29 +0200

    First draft
    First draft

---
+++ exercise_3/Shoping.py
@@ -0,0 +1,12 @@
+class Shoping_Card:
+    def __init__(self,sum=0,Item):
+        self.sum=sum
+        self.item=Item
+        
+        
+class Item: 
+    def __init__(self,name ,price):
+        self.name =name
+        self.price=price
+        
+    
