919af40489ec5251baaf35e718c1c27e82158e27
arsalanrazavi <119681102+arsalanrazavi@users.noreply.github.com>
2024-04-23 10:08:50 +0200

    Add files via upload

---
+++ exercise_3/main.py
@@ -0,0 +1,29 @@
+from calculator import Calculator
+
+def main():
+    # This part Create an instance of the Calculator class
+    calc = Calculator()
+
+    # Test the input value
+    try:
+        result1 = calc.sum(7, 5)
+        print(f"7 + 5 = {result1}")
+
+        result2 = calc.subtract(34, 21)
+        print(f"34 - 21 = {result2}")
+
+        result3 = calc.multiply(54, 2)
+        print(f"54 * 2 = {result3}")
+
+        result4 = calc.divide(144, 2)
+        print(f"144 / 2 = {result4}")
+
+        # in case you divide a number to zero This will raise a ValueError
+        result5 = calc.divide(45, 0)
+        print(f"45 / 0 = {result5}")  # This line won't be reached due to exception
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
