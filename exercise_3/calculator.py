e07815377ec609d860430366230e22c85daedbdd
ZuhalYildirim <114772485+ZuhalYildirim@users.noreply.github.com>
2024-04-25 17:57:39 +0200

    Add files via upload

--- exercise_3/calculator.py
+++ exercise_3/calculator.py
@@ -1,9 +1,5 @@
-
-
-# Here we defined the class
 class Calculator:
-
-    def sum(self, a, b):
+    def add(self, a, b):
         return a + b
 
     def subtract(self, a, b):
@@ -13,7 +9,7 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
-        # in case you divide a number to zero This will raise a ValueError
         if b == 0:
-            raise ValueError("Division by zero is not allowed")
-        return a / b
+            return "Cannot divide by zero"
+        else:
+            return a / b
--- exercise_3/shopping.py
+++ exercise_3/shopping.py
@@ -3,20 +3,17 @@ class ShoppingCart:
     # Initialize the shopping cart with an empty list of items
     def __init__(self):
         self.items = []
-        
 
     # Add an item with a name and quantity to the shopping cart
     def add_item(self, item_name, qty):
         item = (item_name, qty)
         self.items.append(item)
-        
+
     # Remove an item with a specific name from the shopping cart
     def remove_item(self, item_name):
         for item in self.items:
             if item[0] == item_name:
                 self.items.remove(item)
-                self.calculate_total()
-                self.display_item()
                 break
 
     # Calculate and return the total quantity of items in the shopping cart
@@ -26,10 +23,4 @@ class ShoppingCart:
             total += item[1]
         return total
 
-    # Display the current items in the cart and Show the total quantity
-    def display_item(self):
-         print("Current Items in Cart:")
-         for item in self.items:
-           print(item[0], "-", item[1])
-         print(f"{ 'Total Item counts is :' + str(self.calculate_total()) }")
-          
+
